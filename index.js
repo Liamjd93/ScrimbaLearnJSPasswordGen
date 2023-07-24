2700f3ea2c40d3ebe77f4e9e4cedc3a28f0457fc
Liamjd93 <113725214+Liamjd93@users.noreply.github.com>
2023-07-24 18:06:33 +0100

    Length function works
    Created JS function to test that password length will be >= 15

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let symbols = false
+let numbers = false
+let length = 0
+document.getElementById("alert-msg").hidden = true
+
+function pwLength (){
+    length = document.getElementById("pw-length").value
+    alertElement = document.getElementById("alert")
+    console.log(length)
+    console.log(alertElement.childNodes.length)
+    if (length < 15){
+        document.getElementById("alert-msg").hidden = false
+        console.log("less than 15")
+        return
+    }
+    document.getElementById("alert-msg").hidden = true
+    console.log("more than 15")
+    return
+}
+
+function toggleSymbols (){
+    return
+}
+
+function toggleNumbers (){
+    return
+}
+
+function copyToClip(){
+    return
+}
